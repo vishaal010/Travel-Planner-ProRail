1970612047f71f306acd990fdc36370e12b9ba12
vishaal010 <bea_vishaal@hotmail.com>
2024-02-28 11:19:11 +0100

    First section

---
+++ API/ReactivitiesContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public partial class ReactivitiesContext : DbContext
+{
+    public ReactivitiesContext()
+    {
+    }
+
+    public ReactivitiesContext(DbContextOptions<ReactivitiesContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Name=ConnectionStrings:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
